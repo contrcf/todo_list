ca9b1b65f10b7cfd5abdb6871f98eeff81e67d88
contrcf <contrf@cat.comqa>
2022-11-08 21:03:34 -0600

    config

--- todo_list/config.py
+++ todo_list/config.py
@@ -1,10 +1,13 @@
-
-from dynaconf import Dynaconf
+from dynaconf import Dynaconf, Validator
+from pathlib import Path
 
 settings = Dynaconf(
+    # validators=[Validator('NUMBER', 'NAME', must_exist=True)],
     envvar_prefix="DYNACONF",
-    settings_files=['settings.toml', '.secrets.toml'],
+    root_path=Path(__file__).parent,
+    settings_files=["settings.toml", ".secrets.toml"],
+    environment=True,
+    load_dotenv=True,
 )
 
-# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
-# `settings_files` = Load these files in the order.
+ROOT_DIR = Path(__file__).parent

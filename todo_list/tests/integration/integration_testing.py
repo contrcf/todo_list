4c932e3c600aec269c5a7bbd761d87f17faad656
contrcf <contrf@cat.comqa>
2022-11-09 22:52:07 -0600

    testing

--- todo_list/tests/integration/integration_testing.py
+++ todo_list/tests/integration/integration_testing.py
@@ -1,7 +1,8 @@
-from src.to_do_list.cli_methods import helper_functions
-from tests.unit.unit_test import tmp_dir
-from tests.unit.unit_test import df_empty
-from tests.unit.unit_test import df_empty_stored
+from todo_list.src.todo_list.methods import file_functions
+
+from tests.unit.unit_testing import tmp_dir
+from tests.unit.unit_testing import df_empty
+from tests.unit.unit_testing import df_empty_stored
 import pandas as pd
 import pytest
 import shutil
@@ -32,38 +33,38 @@ def df_full(new_row):
 
 
 def test_create_list(tmp_dir, df_empty):
-    helper_functions.create_list("todos")
-    df1 = helper_functions.load_list("todos")
+    file_functions.create_list("todos")
+    df1 = file_functions.load_list("todos")
     pd.testing.assert_frame_equal(df1, df_empty)
 
 
 def test_store_list(tmp_dir, df_empty):
-    helper_functions.store_list(df_empty, "todos")
-    df2 = helper_functions.load_list("todos")
+    file_functions.store_list(df_empty, "todos")
+    df2 = file_functions.load_list("todos")
     pd.testing.assert_frame_equal(df_empty, df2)
 
 
 def test_add_to_list(new_row, df_full, tmp_dir,df_empty_stored):
-    helper_functions.add_to_list("todos", new_row)
-    df1 = helper_functions.load_list("todos")
+    file_functions.add_to_list("todos", new_row)
+    df1 = file_functions.load_list("todos")
     pd.testing.assert_frame_equal(df1, df_full)
 
 
 def test_update_list(df_full_stored):
     df_full_stored.loc[0, "owner"] = "Ivan"
-    helper_functions.update_task_in_list("todos", 0, "owner", "Ivan")
-    df = helper_functions.load_list("todos")
+    file_functions.update_task_in_list("todos", 0, "owner", "Ivan")
+    df = file_functions.load_list("todos")
     pd.testing.assert_frame_equal(df, df_full_stored)
 
 def test_create_multiple_list(tmp_dir, df_empty):
     for i in range(5):
-        helper_functions.create_list(f"todos_{i}")
-    assert len(helper_functions.get_existing_lists()) == 5
+        file_functions.create_list(f"todos_{i}")
+    assert len(file_functions.get_existing_lists()) == 5
 
 def test_update_list_add_column(df_full_stored):
-    helper_functions.create_list("new_list")
-    helper_functions.create_list("other")
-    helper_functions.update_task_in_list("new_list", 0, "new_column", "other")
-    df_add_column = helper_functions.load_list("new_list")
-    df_other = helper_functions.load_list("other")
+    file_functions.create_list("new_list")
+    file_functions.create_list("other")
+    file_functions.update_task_in_list("new_list", 0, "new_column", "other")
+    df_add_column = file_functions.load_list("new_list")
+    df_other = file_functions.load_list("other")
     assert df_add_column.shape[1] == df_other.shape[1] + 1
--- todo_list/tests/unit/unit_testing.py
+++ todo_list/tests/unit/unit_testing.py
@@ -1,4 +1,4 @@
-from src.to_do_list.cli_methods import helper_functions
+from src.todo_list.methods import file_functions
 import pandas as pd
 import pytest
 import shutil
@@ -7,7 +7,7 @@ import shutil
 @pytest.fixture(scope="function")
 def tmp_dir(tmpdir_factory):
     my_tmpdir = tmpdir_factory.mktemp("pytestdata")
-    helper_functions.PATH_TO_DATA = str(my_tmpdir)
+    file_functions.PATH_TO_DATA = str(my_tmpdir)
     yield my_tmpdir
     shutil.rmtree(str(my_tmpdir))
 
@@ -25,26 +25,26 @@ def df_empty_stored(tmp_dir, df_empty):
 
 
 def test_check_list_exists(df_empty_stored):
-    assert helper_functions.check_list_exists("todos") == True
+    assert file_functions.check_list_exists("todos") == True
 
 
 def test_get_list_path():
-    assert helper_functions.get_list_path("todos") == \
-        helper_functions.PATH_TO_DATA + "/" + "todos.csv"
+    assert file_functions.get_list_path("todos") == \
+        file_functions.PATH_TO_DATA + "/" + "todos.csv"
 
 
 def test_get_filename():
-    assert helper_functions.get_list_filename("data") == "data.csv"
+    assert file_functions.get_list_filename("data") == "data.csv"
 
 
 def test_check_list_exists_fail(tmp_dir):
-    assert helper_functions.check_list_exists("todos") == False
+    assert file_functions.check_list_exists("todos") == False
 
 
 def test_load_list(df_empty_stored, tmp_dir):
-    df = helper_functions.load_list("todos")
+    df = file_functions.load_list("todos")
     pd.testing.assert_frame_equal(df_empty_stored, df)
 
 
 def test_check_number_of_files(df_empty_stored):
-    assert len(helper_functions.get_existing_lists()) == 1
+    assert len(file_functions.get_existing_lists()) == 1
